28310ba0390a9d963dc11ef2fb19e50645abd5ef
marannta_page <laura.bedoya194@gmail.com>
2022-11-15 11:05:09 +1100

    primer commit base de datos conectada

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  controllers: [AppController],
+  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 4321,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'marannta_api',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+})
+export class AppModule {}
